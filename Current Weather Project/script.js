d4af57d61073f2674e11f2ed596abe7f98c9483a
jayantp2003 <jayantparakh2003@gmail.com>
2023-03-19 02:27:44 +0530

    current weather project

---
+++ Current Weather Project/script.js
@@ -0,0 +1,63 @@
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'cb95689d72msh25dd3feafa596f4p1e3dcbjsn863ff41404e6',
+        'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+    }
+};
+// var city = "New York"
+
+const getWeather = (city) => {
+    if (city ==="")
+    {
+        city = "Chennai";
+        console.log("Enter a city");
+    }
+    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
+        .then(response => response.json())
+        .then(response => {
+            console.log(response)
+            document.getElementById("cityName").innerHTML = city
+            document.getElementById("temp").innerHTML = response.temp + "<span>&#8451;</span>"
+            document.getElementById("temp-max").innerHTML = response.max_temp + "<span>&#8451;</span>"
+            document.getElementById("temp-min").innerHTML = response.min_temp + "<span>&#8451;</span>"
+
+            document.getElementById("humidity").innerHTML = response.humidity + "%"
+            document.getElementById("wind-speed").innerHTML = response.wind_speed + " Km/hr"
+            document.getElementById("wind-direction").innerHTML = response.wind_degrees
+
+            document.getElementById("feels-like").innerHTML = response.feels_like + "<span>&#8451;</span>"
+            document.getElementById("sunrise").innerHTML = response.sunrise
+            document.getElementById("sunset").innerHTML = response.sunset
+
+
+        })
+        .catch(err => console.error(err));
+}
+
+submit.addEventListener("click", (e) => {
+    e.preventDefault()
+    console.log(city.value)
+    getWeather(city.value)
+}
+)
+
+delhi.addEventListener("click", (e) => {
+    e.preventDefault()
+    getWeather("Delhi")
+}
+)
+
+mumbai.addEventListener("click", (e) => {
+    e.preventDefault()
+    getWeather("Mumbai")
+}
+)
+
+banglore.addEventListener("click", (e) => {
+    e.preventDefault()
+    getWeather("Banglore")
+}
+)
+
+getWeather("Chennai")
---
+++ README.md
@@ -0,0 +1,4 @@
+<h1>Current Weather presentation using API requests</h1>
+<hr>
+
+<p>This is a basic project for understanding how api requests works and how we can implement them into our website to show data. In this project I have made a website which uses api requests to get the weather and present it. I have customised this in such a way that you can get the weather by entering the city.</p>
